b732b6eb18e308e169e509bfcadc27c95e801569
Kouser Ahamed <kouserahamed.cse.diu@gmail.com>
2026-07-20 15:34:17 +0600

    feat: add AIHealthAssistant, FAQ, HealthcareStats, and HowItWorks sections for enhanced user experience

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,18 +1,29 @@
-
+import AIHealthAssistantSection from "@/components/homepage/AIHealthAssistantSection";
+import FAQSection from "@/components/homepage/FAQSection";
 import FeaturesSection from "@/components/homepage/FeaturesSection";
+import HealthcareStatsSection from "@/components/homepage/HealthcareStatsSection";
 import HeroSection from "@/components/homepage/HeroSection";
+import HowItWorksSection from "@/components/homepage/HowItWorksSection";
 import PatientReviewsSection from "@/components/homepage/PatientReviewsSection";
 import TopRatedDoctors from "@/components/homepage/TopRatedDoctors";
-
 
 export default function Home() {
   return (
     <div>
       <HeroSection />
       <TopRatedDoctors />
+
+      <AIHealthAssistantSection />
+
       <FeaturesSection />
-      
+
+      <HowItWorksSection />
+
+      <HealthcareStatsSection />
+
       <PatientReviewsSection />
+
+      <FAQSection />
     </div>
   );
 }
